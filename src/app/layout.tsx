7d00130932272ffb9080f7b63d0750bfee0d507e
Roman <roman.pr@skillwebquality.com>
2024-09-17 14:33:45 +0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { NextUIProvider } from "@nextui-org/react";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
   );
